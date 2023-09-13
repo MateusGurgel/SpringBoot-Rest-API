71367fb2c231b37771bf6fb95c47b24a1d3ec0ea
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-09-13 09:45:53 -0300

    feat: math route created

--- src/main/java/com/gurgel/apigateway/MathController.java
+++ src/main/java/com/gurgel/apigateway/MathController.java
@@ -9,9 +9,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
     @RequestMapping(value="/math/{num}/{num2}", method=RequestMethod.GET)
-    public float calculator(@PathVariable("num") Float num,
-                            @PathVariable("num2") Float num2){
+    public double calculator(@PathVariable("num") String num,
+                            @PathVariable("num2") String num2
+    ) throws Exception{
 
-        return num + num2;
+        if(!isNumber(num) || !isNumber(num2)){
+            throw new Exception();
+        }
+
+
+        return convertToDouble(num) + convertToDouble(num2);
+    }
+
+    private double convertToDouble(String strNumber){
+        if (strNumber == null) return 0D;
+
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumber(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumber(String strNumber){
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
