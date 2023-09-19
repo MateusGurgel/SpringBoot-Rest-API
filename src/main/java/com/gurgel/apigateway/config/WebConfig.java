ea6b5c33ac255032f4fbf68ea8528cdade6febba
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-09-19 09:39:55 -0300

    feat: Content negotiation implemented

---
+++ src/main/java/com/gurgel/apigateway/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.gurgel.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        //Via queryParams
+        /*
+        * configurer.favorParameter(true)
+                .parameterName("MediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+        */
+
+        //Header
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+
+    }
+}
