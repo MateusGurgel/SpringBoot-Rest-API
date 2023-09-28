457dcc72e80ae1e7808e2857828b5bb19a98c764
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-09-28 20:30:13 -0300

    feat: Cors implemented via application.properties

--- src/main/java/com/gurgel/apigateway/config/WebConfig.java
+++ src/main/java/com/gurgel/apigateway/config/WebConfig.java
@@ -1,12 +1,26 @@
 package com.gurgel.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        for(String origin: allowedOrigins.split(",")) {
+            registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins(origin);
+        }
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
